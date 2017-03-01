a4da1af80197a689ce4993659f480d58f28b2478
cv14 <spaudyal@gmail.com>
2017-03-01 13:09:51 -0500

    ADD 02

---
+++ chapter_2/02_exercise.c
@@ -0,0 +1,28 @@
+/***********************	
+	Saurav Paudyal
+	CV14
+	MUTD209
+
+Write a loop equivalent to the for
+loop above without using && or || .
+
+*************************/
+#include <stdio.h>
+
+int main(void){
+  
+	printf("type cat 02_exercise.c\n"); 
+/*
+	 int i=0;
+         while(i<lim-1) {
+              if((c=getchar()) != '\n') {
+                   if(c != EOF) {
+                         s[i] = c;
+                   }
+              }
+              i++;
+         }
+*/
+
+	return 0;
+}
